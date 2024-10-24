83d9eb403ce72849754620f25d07feea2f27536f
Jaime Simeón Palomar Blumenthal <jpalomarbl@uoc.edu.es>
2024-10-24 10:30:13 +0100

    Fixed issue with mobile width

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -11,6 +11,13 @@
  */
 
 const cards = document.querySelectorAll('.card');
+const cardInfos = document.querySelectorAll('.card__info');
+
+const shortInfos = [
+  'Discover the hidden gem of Abades, Tenerife! This tranquil coastal village offers crystal-clear waters, golden sands, and stunning views. Perfect for sunbathing, snorkeling, or simply relaxing, Abades is a peaceful escape from crowded spots.',
+  'Explore the awe-inspiring Teide National Park, home to Spain’s highest peak and a UNESCO World Heritage Site. Whether hiking its volcanic trails or taking the cable car to the summit, you’ll be captivated by the otherworldly landscapes and breathtaking views',
+  'Step back in time in San Cristóbal de La Laguna, a UNESCO World Heritage city rich in history and colonial architecture. Wander through its charming streets, admire beautifully preserved churches and palaces, and experience the vibrant culture of this picturesque town.'
+]
 
 cards.forEach(card => {
   card.addEventListener('click', () => {
@@ -21,3 +28,9 @@ cards.forEach(card => {
     }
   });
 });
+
+if (window.innerWidth <= 411) {
+  cardInfos.forEach((cardInfo, index) => {
+    cardInfo.innerHTML =  shortInfos[index];
+  });
+}
